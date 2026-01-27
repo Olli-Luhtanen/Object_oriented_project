2768d496024688f52b68f8c70a726b5dbffe9bd5
Olli Luhtanen <olli.luhtanen@gmail.com>
2026-01-27 14:24:37 +0200

    Add project files.

---
+++ Object_oriented_project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Object_oriented_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
