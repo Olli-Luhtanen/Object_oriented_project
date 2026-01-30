5595c98b7b1e15dcabaf7b21b596fc0ae4c7cd7b
Olli Luhtanen <olli.luhtanen@gmail.com>
2026-01-30 22:30:45 +0200

    Innicial thoughts

---
+++ Object_oriented_project/Tapahtuma.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Object_oriented_project
+{
+    public class Tapahtuma
+    {
+        private int maara;
+        private int id;
+        private string kategoria;
+        private string paivamaara;
+        private int type;
+    }
+}
---
+++ Object_oriented_project/tili.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Object_oriented_project
+{
+    public class Tili
+    {
+        private float _saldo;
+        private string _nimi;
+    }
+}
